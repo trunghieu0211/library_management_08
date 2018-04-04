b7a5e025fc237712c5e9f52403f2ba32f3352874
trunghieu0211 <trunghieugx0211@gmail.com>
2018-04-04 19:51:52 +0700

    static pages

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem "sass-rails", "~> 5.0"
 gem "sqlite3"
 gem "turbolinks", "~> 5"
 gem "uglifier", ">= 1.3.0"
+gem "font-awesome-rails"
+gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
 
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,29 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
+//= require jquery3
+//= require jquery_ujs
 //= require rails-ujs
 //= require turbolinks
-//= require_tree .
+//= require vendor/bootstrap.min
+//= require vendor/jquery
+//= require vendor/modernizr.js
+//= require 3d-book-showcase.js
+//= require appear.js
+//= require bigslide.js
+//= require bxslider.js
+//= require classie.js
+//= require contact-form.js
+//= require countdown.js
+//= require datepicker.js
+//= require isotope.pkgd.js
+//= require jquery-ui.js
+//= require main.js
+//= require mcustom-scrollbar.js
+//= require owl-carousel.js
+//= require parallax.js
+//= require prettyPhoto.js
+//= require sticky.js
+//= require timeliner.js
+//= require turn.js
+//= require wow-min.js
+//= require colorswitcher
+//= require cookie
+
